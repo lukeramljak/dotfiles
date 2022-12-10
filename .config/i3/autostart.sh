fd0186c4ce3ee2186027b2612f1aab2eabeeb8ed
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2022-12-10 13:54:16 +0800

    add nitrogen sleep/specify picom config

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -3,11 +3,11 @@
 # compositor
 killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-picom &
+picom --config ~/.config/picom/picom.conf &
 
 ~/.config/i3/polybar/launch.sh &
 
-nitrogen --restore &
+nitrogen --restore; sleep 1 &
 ~/.local/bin/autotiling &
 dunst &
 unclutter &
